eada9a56032a7e070043a865355a78527b7117b3
jes <jesravnbol@hotmail.com>
2022-01-13 14:50:26 +0100

    Testing - WIP.

---
+++ tests/test_feature_engineering.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+from src.feature_engineering import get_mean_within_category
+
+
+def test_get_mean_within_category():
+
+    input_df = pd.DataFrame({
+        "group": ["a", "b", "a", "b", "c"],
+        "value": [1, 3, 3, 2, 4],
+    })
+    expected_result = np.array([2, 2.5, 2, 2.5, 4])
+    assert (get_mean_within_category(input_df, "group", "value").values == expected_result).all()
