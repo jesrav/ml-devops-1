f84e3e4aaaa33e06baa38b44f7f585eab774784c
jes <jesravnbol@hotmail.com>
2022-01-17 22:20:21 +0100

    Added barchart for marital status feature  and distribution plot for total transaction feature..

--- plotting.py
+++ plotting.py
@@ -14,10 +14,11 @@ os.environ["QT_QPA_PLATFORM"] = "offscreen"
 
 def plot_univariate_hist(dataf: pd.DataFrame, col_name: str, out_path: str) -> None:
     """
-    Plot histogram of churn.
+    Plot histogram.
 
     input:
         dataf: pandas dataframe with preprocessed data. Must include chrun column.
+        col_name: Name of column to make a histogram of.
         out_path: String with outpath for plot.
 
     output:
@@ -27,6 +28,45 @@ def plot_univariate_hist(dataf: pd.DataFrame, col_name: str, out_path: str) -> N
     plt.figure(figsize=(20, 10))
     plt.xlabel(col_name)
     fig = dataf[col_name].hist().get_figure()
+    fig.savefig(out_path)
+
+
+def plot_univariate_dist(dataf: pd.DataFrame, col_name: str, out_path: str) -> None:
+    """
+    Plot distribution.
+
+    input:
+        dataf: pandas dataframe with preprocessed data. Must include chrun column.
+        col_name: Name of column to make a histogram of.
+        out_path: String with outpath for plot.
+
+    output:
+            None
+            :rtype: object
+    """
+    plt.figure(figsize=(20, 10))
+    plt.xlabel(col_name)
+    plt.figure(figsize=(20, 10))
+    dist_fit = sns.distplot(dataf[col_name]).get_figure()
+    dist_fit.savefig(out_path)
+
+
+def plot_bar_chart(dataf: pd.DataFrame, col_name: str, out_path: str) -> None:
+    """
+    Plot bar chart.
+
+    input:
+        dataf: pandas dataframe with preprocessed data. Must include chrun column.
+        col_name: Name of column to make a histogram of.
+        out_path: String with outpath for plot.
+
+    output:
+            None
+            :rtype: object
+    """
+    plt.figure(figsize=(20, 10))
+    plt.xlabel(col_name)
+    fig = dataf[col_name].value_counts('normalize').plot(kind='bar').get_figure()
     fig.savefig(out_path)
 
 
